ab4f8e5cfc7ab176c4efbb8ecc3119d5f7d6a34c
Dellon <jy.dellon@gmail.com>
2025-01-04 12:29:25 +0100

    maj

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -478,50 +478,314 @@
 
 
 
+// import { v4 as uuidv4 } from 'uuid';
+// import React, { useState, useEffect } from 'react';
+// import { Link, useLocation } from 'react-router-dom';
+// import axios from 'axios';
+// import { useTranslation } from 'react-i18next';
+// import './Navbar.css';
+
+// function Navbar({ onLinkClick }) {
+//   const location = useLocation();
+//   const { t } = useTranslation();
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+//   const [accessGranted, setAccessGranted] = useState(false); // Utilisé pour contrôler l'accès au tableau de bord
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   // Fonction pour vérifier si un lien est actif
+//   const isActiveLink = (path) => location.pathname === path;
+
+//   // Gère l'ouverture et la fermeture du menu
+//   const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+//   const closeMenu = () => {
+//     setIsMenuOpen(false);
+//     if (onLinkClick) onLinkClick();
+//   };
+
+
+
+  
+//   const openModal = () => setIsModalOpen(true);
+//   const closeModal = () => setIsModalOpen(false);
+
+//   // Vérifie l'accès au tableau de bord via une requête backend
+//   useEffect(() => {
+//     axios
+//       .get('apiaeonix-production.up.railway.app/api/dashboard', { withCredentials: true })
+//       .then((response) => {
+//         if (response.status === 200) {
+//           setAccessGranted(true); // Affiche le lien vers le tableau de bord si autorisé
+//         }
+//       })
+//       .catch(() => {
+//         setAccessGranted(false); // Masque le lien si non autorisé
+//       });
+//   }, []);
+
+
+
+
+
+
+
+//   function getDeviceUUID() {
+//     let uuid = localStorage.getItem('deviceUUID');
+//     if (!uuid) {
+//       uuid = uuidv4(); // Génère un UUID unique
+//       localStorage.setItem('deviceUUID', uuid);
+//     }
+//     return uuid;
+//   }
+
+
+
+
+
+
+
+//   return (
+//     <div className="navbar-container">
+//       {/* Menu Burger */}
+//       <div className="miseEnPage">
+//         <div className="navbar-header">
+//           <button
+//             className="menu-toggle"
+//             onClick={openModal}
+//             aria-label="Toggle menu"
+//           >
+//             ☰
+//           </button>
+//         </div>
+//       </div>
+
+//       {/* Modale pour les petits écrans */}
+//       {isModalOpen && (
+//         <div className="modalXXX" onClick={closeModal}>
+//           <div className="modalXXX-content">
+//             <nav className="modalXXX-menu">
+//               <ul>
+//                 <li>
+//                   <Link
+//                     to="/"
+//                     className={isActiveLink('/') ? 'active' : ''}
+//                     onClick={closeModal}
+//                   >
+//                     {t('navbar.home')}
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/a-propos"
+//                     className={isActiveLink('/a-propos') ? 'active' : ''}
+//                     onClick={closeModal}
+//                   >
+//                     {t('navbar.about')}
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/nos-services"
+//                     className={isActiveLink('/nos-services') ? 'active' : ''}
+//                     onClick={closeModal}
+//                   >
+//                     {t('navbar.services')}
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/portfolio"
+//                     className={isActiveLink('/portfolio') ? 'active' : ''}
+//                     onClick={closeModal}
+//                   >
+//                     {t('navbar.portfolio')}
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/contact"
+//                     className={isActiveLink('/contact') ? 'active' : ''}
+//                     onClick={closeModal}
+//                   >
+//                     {t('navbar.contact')}
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link
+//                     to="/rgpd"
+//                     className={isActiveLink('/rgpd') ? 'active' : ''}
+//                     onClick={closeModal}
+//                   >
+//                     {t('navbar.rgpd')}
+//                   </Link>
+//                 </li>
+//                 {/* Affiche le lien vers le tableau de bord si accès autorisé */}
+//                 {accessGranted && (
+//                   <li>
+//                     <Link
+//                       to="/dashboard"
+//                       className={isActiveLink('/dashboard') ? 'active' : ''}
+//                       onClick={closeModal}
+//                     >
+//                       {t('navbar.dashboard')}
+//                     </Link>
+//                   </li>
+//                 )}
+//               </ul>
+//             </nav>
+//           </div>
+//         </div>
+//       )}
+
+//       {/* Navbar principale */}
+//       <nav className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
+//         <ul>
+//           <li>
+//             <Link
+//               to="/"
+//               className={isActiveLink('/') ? 'active' : ''}
+//               onClick={closeMenu}
+//             >
+//               {t('navbar.home')}
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               to="/a-propos"
+//               className={isActiveLink('/a-propos') ? 'active' : ''}
+//               onClick={closeMenu}
+//             >
+//               {t('navbar.about')}
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               to="/nos-services"
+//               className={isActiveLink('/nos-services') ? 'active' : ''}
+//               onClick={closeMenu}
+//             >
+//               {t('navbar.services')}
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               to="/portfolio"
+//               className={isActiveLink('/portfolio') ? 'active' : ''}
+//               onClick={closeMenu}
+//             >
+//               {t('navbar.portfolio')}
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               to="/contact"
+//               className={isActiveLink('/contact') ? 'active' : ''}
+//               onClick={closeMenu}
+//             >
+//               {t('navbar.contact')}
+//             </Link>
+//           </li>
+//           <li>
+//             <Link
+//               to="/rgpd"
+//               className={isActiveLink('/rgpd') ? 'active' : ''}
+//               onClick={closeMenu}
+//             >
+//               {t('navbar.rgpd')}
+//             </Link>
+//           </li>
+//           {/* Affiche le lien vers le tableau de bord si accès autorisé */}
+//           {accessGranted && (
+//             <li>
+//               <Link
+//                 to="/dashboard"
+//                 className={isActiveLink('/dashboard') ? 'active' : ''}
+//                 onClick={closeMenu}
+//               >
+//                 {t('navbar.dashboard')}
+//               </Link>
+//             </li>
+//           )}
+//         </ul>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 import './Navbar.css';
+import { v4 as uuidv4 } from 'uuid'; // Générateur d'UUID
 
 function Navbar({ onLinkClick }) {
   const location = useLocation();
   const { t } = useTranslation();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [accessGranted, setAccessGranted] = useState(false); // Utilisé pour contrôler l'accès au tableau de bord
+  const [accessGranted, setAccessGranted] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  // Fonction pour vérifier si un lien est actif
-  const isActiveLink = (path) => location.pathname === path;
-
-  // Gère l'ouverture et la fermeture du menu
-  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-    if (onLinkClick) onLinkClick();
+  // Fonction pour générer ou récupérer le UUID
+  const getDeviceUUID = () => {
+    let uuid = localStorage.getItem('deviceUUID');
+    if (!uuid) {
+      uuid = uuidv4(); // Génère un UUID unique
+      localStorage.setItem('deviceUUID', uuid);
+    }
+    return uuid;
   };
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
-
-  // Vérifie l'accès au tableau de bord via une requête backend
+  // Vérifie l'accès au tableau de bord
   useEffect(() => {
+    const uuid = getDeviceUUID(); // Récupère le UUID de l'appareil
+
     axios
-      .get('apiaeonix-production.up.railway.app/api/dashboard', { withCredentials: true })
+      .get('http://localhost:8000/api/dashboard', {
+        headers: { 'X-Device-ID': uuid }, // Envoie le UUID dans l'en-tête
+        withCredentials: true,
+      })
       .then((response) => {
         if (response.status === 200) {
-          setAccessGranted(true); // Affiche le lien vers le tableau de bord si autorisé
+          setAccessGranted(true);
         }
       })
       .catch(() => {
-        setAccessGranted(false); // Masque le lien si non autorisé
+        setAccessGranted(false); // Refuse l'accès si non autorisé
       });
   }, []);
 
+  const isActiveLink = (path) => location.pathname === path;
+
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    if (onLinkClick) {
+      onLinkClick();
+    }
+  };
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <div className="navbar-container">
-      {/* Menu Burger */}
       <div className="miseEnPage">
         <div className="navbar-header">
           <button
@@ -534,7 +798,6 @@ function Navbar({ onLinkClick }) {
         </div>
       </div>
 
-      {/* Modale pour les petits écrans */}
       {isModalOpen && (
         <div className="modalXXX" onClick={closeModal}>
           <div className="modalXXX-content">
@@ -594,7 +857,6 @@ function Navbar({ onLinkClick }) {
                     {t('navbar.rgpd')}
                   </Link>
                 </li>
-                {/* Affiche le lien vers le tableau de bord si accès autorisé */}
                 {accessGranted && (
                   <li>
                     <Link
@@ -612,7 +874,6 @@ function Navbar({ onLinkClick }) {
         </div>
       )}
 
-      {/* Navbar principale */}
       <nav className={`navbar-menu ${isMenuOpen ? 'open' : ''}`}>
         <ul>
           <li>
@@ -669,7 +930,6 @@ function Navbar({ onLinkClick }) {
               {t('navbar.rgpd')}
             </Link>
           </li>
-          {/* Affiche le lien vers le tableau de bord si accès autorisé */}
           {accessGranted && (
             <li>
               <Link
@@ -688,5 +948,3 @@ function Navbar({ onLinkClick }) {
 }
 
 export default Navbar;
-
-
