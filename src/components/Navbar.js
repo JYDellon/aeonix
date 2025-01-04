a924e11fb482ae869f1890cb2704ddfcb9e42bc4
Dellon <jy.dellon@gmail.com>
2025-01-04 08:42:14 +0100

    maj

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -278,7 +278,7 @@ function Navbar({ onLinkClick }) {
 
   useEffect(() => {
       // Envoie la requête en incluant les cookies avec 'withCredentials: true'
-      axios.get('https://apiaeonix-production.up.railway.app/api/dashboard', { withCredentials: true })
+      axios.get("https://apiaeonix-production.up.railway.app/api/dashboard", { withCredentials: true })
       .then(response => {
         if (response.status === 200) {
           setAccessGranted(true);  // Si l'accès est autorisé, on affiche le tableau de bord
